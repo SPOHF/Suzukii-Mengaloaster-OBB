da53f1172c3a6e4f7be0bf6fd0aa3ea97bc8b2fc
ChristianSalz <49674749+ChristianSalz@users.noreply.github.com>
2026-06-02 10:31:38 +0200

    Feature\ added dynamic gpu / cpu selection

--- train.py
+++ train.py
@@ -1,4 +1,15 @@
+import torch
 from ultralytics import YOLO
+
+def get_device():
+    if torch.cuda.is_available():
+        return "cuda"
+    elif torch.backends.mps.is_available():
+        return "mps"
+    else:
+        return "cpu"
+
+device = get_device()
 
 model = YOLO("yolo26n.pt")
 
@@ -7,6 +18,6 @@ model.train(
     epochs=100,
     imgsz=640,
     batch=16,
-    device="mps",
+    device=device,
     patience=20
 )
